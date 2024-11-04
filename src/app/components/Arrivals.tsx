b34a8d3cda13a9d79fc2e32d0fee42505709b2b8
QareerShah <queensyeda16@gmail.com>
2024-11-04 20:28:49 +0500

    first commit

--- src/app/components/Arrivals.tsx
+++ src/app/components/Arrivals.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image';
             <h2 className='arrival-sec-h2'>New Arrivals</h2>
             <div className='card-container'>
                   <div className="card">
-                    <Image src="/images/tissot-watches.png" alt='card-image' width={320} height={320} className='card-image'/>
+                    <Image src="/images/tissot2.png" alt='card-image' width={320} height={320} className='card-image'/>
                     <h2>Tissot</h2>
                     <p className='card-pricing'>&#36;3,988</p>
                     <button className='arrivals-btn'>Discover</button>
